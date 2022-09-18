10116c9fe5e654a9885a93434a2e73359fd731d4
Jxiang2 <xjyhenry@gmail.com>
2022-09-17 22:13:11 -0400

    set up employee

---
+++ src/main/java/org/workaxle/domain/Schedule.java
@@ -0,0 +1,32 @@
+package org.workaxle.domain;
+
+import lombok.Data;
+import org.optaplanner.core.api.domain.solution.PlanningEntityCollectionProperty;
+import org.optaplanner.core.api.domain.solution.PlanningScore;
+import org.optaplanner.core.api.domain.solution.PlanningSolution;
+import org.optaplanner.core.api.domain.solution.ProblemFactCollectionProperty;
+import org.optaplanner.core.api.domain.valuerange.ValueRangeProvider;
+import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
+
+import java.util.List;
+
+@Data
+@PlanningSolution
+public class Schedule {
+
+    @PlanningScore
+    private HardSoftScore score;
+
+    @ValueRangeProvider(id = "employeeRange")
+    @ProblemFactCollectionProperty
+    private List<EmployeeGroup> employeeGroupList;
+
+    @PlanningEntityCollectionProperty
+    private List<ShiftAssignment> shiftAssignmentList;
+
+    public Schedule(List<EmployeeGroup> employeeGroupList, List<ShiftAssignment> shiftAssignmentList) {
+        this.employeeGroupList = employeeGroupList;
+        this.shiftAssignmentList = shiftAssignmentList;
+    }
+
+}
