eb575c7ef71e1fd0bde1b4ed66cef2721745ef9a
Jxiang2 <xjyhenry@gmail.com>
2022-09-19 21:24:02 -0400

    refine docs

--- README.md
+++ README.md
@@ -6,22 +6,170 @@ Schedule shifts for all employees in a department
 
 ## Requirements:
 
-1. Each shift is assigned to 2 employees
-2. Try to distribute the shifts evenly to employees ✅
-3. No employeeGroup works more than 1 shift in 1 day ✅
-4. No employeeGroup works more than 1 shift in 12 hours ✅
+1. Each shift is assigned to 1 ev role and 1 Design role
+2. Try to distribute the shifts evenly to all employees
+3. No employeeGroup works more than 1 shift in 1 day
+4. No employeeGroup works more than 1 shift in 12 hours
 
 ### Models:
 
 1. Shift
-2. Shift Assignment
-3. Employee
+2. Shift Assignment (Planning entity)
+3. EmployeeGroup
+4. Employee
+5. Schedule (Planning solution)
 
-### Explanation:
+### Inputs:
 
-1. One shift entity can be used to create multiple shiftAssignment entities.
-2. Multiple shiftAssignment entity can be assigned to one employeeGroup.
-3. The number of employees per shift is known. Thus, Employee should be a planning variable.
+1. employees: all employees in a specified group
+2. roles: all roles required to fulfill the shifts
+3. shifts: all shifts for scheduling
+4. startDate: start date of the first shift
+5. endDate: end date of the last shift
+
+#### e.g.
+
+```json
+{
+  "employees": [
+    {
+      "id": 1,
+      "name": "user 1",
+      "roles": [
+        "Dev"
+      ]
+    },
+    {
+      "id": 2,
+      "name": "user 2",
+      "roles": [
+        "Design"
+      ]
+    },
+    {
+      "id": 3,
+      "name": "user 3",
+      "roles": [
+        "Dev"
+      ]
+    },
+    {
+      "id": 4,
+      "name": "user 4",
+      "roles": [
+        "Design"
+      ]
+    },
+    {
+      "id": 5,
+      "name": "user 5",
+      "roles": [
+        "Clean"
+      ]
+    },
+    {
+      "id": 6,
+      "name": "user 6",
+      "roles": [
+        "Dev"
+      ]
+    },
+    {
+      "id": 7,
+      "name": "user 7",
+      "roles": [
+        "Design"
+      ]
+    },
+    {
+      "id": 8,
+      "name": "user 8",
+      "roles": [
+        "Dev"
+      ]
+    },
+    {
+      "id": 9,
+      "name": "user 9",
+      "roles": [
+        "Design"
+      ]
+    },
+    {
+      "id": 10,
+      "name": "user 10",
+      "roles": [
+        "Dev"
+      ]
+    },
+    {
+      "id": 11,
+      "name": "user 11",
+      "roles": [
+        "Design",
+        "Dev",
+        "???"
+      ]
+    },
+    {
+      "id": 12,
+      "name": "user 12",
+      "roles": [
+        "???",
+        " ? "
+      ]
+    }
+  ],
+  "roles": [
+    "Dev",
+    "Design",
+    "Clean"
+  ],
+  "shifts": [
+    {
+      "id": 1,
+      "name": "develop new features",
+      "startAt": "09:00",
+      "endAt": "12:00",
+      "requiredRoles": {
+        "Dev": 1,
+        "Design": 1
+      }
+    },
+    {
+      "id": 2,
+      "name": "develop new features",
+      "startAt": "12:00",
+      "endAt": "15:00",
+      "requiredRoles": {
+        "Dev": 1,
+        "Design": 1
+      }
+    },
+    {
+      "id": 3,
+      "name": "develop new features",
+      "startAt": "15:00",
+      "endAt": "18:00",
+      "requiredRoles": {
+        "Dev": 1,
+        "Design": 1
+      }
+    },
+    {
+      "id": 4,
+      "name": "clean up",
+      "startAt": "18:00",
+      "endAt": "23:00",
+      "requiredRoles": {
+        "Clean": 1
+      }
+    }
+  ],
+  "startDate": "2022-11-21",
+  "endDate": "2022-11-25"
+}
+```
 
 
 
--- src/main/java/org/workaxle/ScheduleApp.java
+++ src/main/java/org/workaxle/ScheduleApp.java
@@ -4,17 +4,11 @@ import org.optaplanner.core.api.solver.Solver;
 import org.optaplanner.core.api.solver.SolverFactory;
 import org.optaplanner.core.config.solver.SolverConfig;
 import org.workaxle.dao.Data;
-import org.workaxle.domain.EmployeeGroup;
 import org.workaxle.domain.Schedule;
-import org.workaxle.domain.Shift;
 import org.workaxle.domain.ShiftAssignment;
 import org.workaxle.solver.ScheduleConstraintProvider;
 
 import java.time.Duration;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class ScheduleApp {
@@ -52,97 +46,6 @@ public class ScheduleApp {
             }
             currentShiftPerDay += 1;
         }
-    }
-
-    private static Schedule generateData() {
-        // total shifts: 5 (days) * 4 (shiftsPerDay) = 20
-        // total shifts per employeeGroup: 20 / 4 = 5
-
-        long i = 1L;
-        Shift s1 = new Shift(
-            i++,
-            "shift A",
-            LocalTime.of(9, 0),
-            LocalTime.of(12, 0),
-            new HashMap<>() {{
-                put("Design", 1);
-                put("Dev", 1);
-            }}
-        );
-        Shift s2 = new Shift(
-            i++,
-            "shift B",
-            LocalTime.of(12, 0),
-            LocalTime.of(15, 0),
-            new HashMap<>() {{
-                put("Design", 1);
-                put("Dev", 1);
-            }}
-        );
-        Shift s3 = new Shift(
-            i++,
-            "shift C",
-            LocalTime.of(15, 0),
-            LocalTime.of(18, 0),
-            new HashMap<String, Integer>() {{
-                put("Design", 1);
-                put("Dev", 1);
-            }}
-        );
-        Shift s4 = new Shift(
-            i++,
-            "shift D",
-            LocalTime.of(18, 0),
-            LocalTime.of(23, 0),
-            new HashMap<>() {{
-                put("Clean", 1);
-            }}
-        );
-
-        List<EmployeeGroup> employeeGroupList = new ArrayList<>();
-        employeeGroupList.add(new EmployeeGroup("1", new HashMap<>() {{
-            put("Dev", 1);
-            put("Design", 1);
-        }}));
-        employeeGroupList.add(new EmployeeGroup("2", new HashMap<>() {{
-            put("Dev", 1);
-            put("Design", 1);
-        }}));
-        employeeGroupList.add(new EmployeeGroup("3", new HashMap<>() {{
-            put("Dev", 1);
-            put("Design", 1);
-        }}));
-        employeeGroupList.add(new EmployeeGroup("4", new HashMap<>() {{
-            put("Clean", 1);
-        }}));
-
-        List<ShiftAssignment> shiftAssignmentList = new ArrayList<>();
-        shiftAssignmentList.add(new ShiftAssignment("1cwec", s1, LocalDate.of(2022, 11, 21)));
-        shiftAssignmentList.add(new ShiftAssignment("cwe2", s2, LocalDate.of(2022, 11, 21)));
-        shiftAssignmentList.add(new ShiftAssignment("eded3", s3, LocalDate.of(2022, 11, 21)));
-        shiftAssignmentList.add(new ShiftAssignment("cwc4", s4, LocalDate.of(2022, 11, 21)));
-
-        shiftAssignmentList.add(new ShiftAssignment("5dwed", s1, LocalDate.of(2022, 11, 22)));
-        shiftAssignmentList.add(new ShiftAssignment("cwce6", s2, LocalDate.of(2022, 11, 22)));
-        shiftAssignmentList.add(new ShiftAssignment("wedew7", s3, LocalDate.of(2022, 11, 22)));
-        shiftAssignmentList.add(new ShiftAssignment("8cew", s4, LocalDate.of(2022, 11, 22)));
-
-        shiftAssignmentList.add(new ShiftAssignment("9wc", s1, LocalDate.of(2022, 11, 23)));
-        shiftAssignmentList.add(new ShiftAssignment("1de0", s2, LocalDate.of(2022, 11, 23)));
-        shiftAssignmentList.add(new ShiftAssignment("1cew1", s3, LocalDate.of(2022, 11, 23)));
-        shiftAssignmentList.add(new ShiftAssignment("12", s4, LocalDate.of(2022, 11, 23)));
-
-        shiftAssignmentList.add(new ShiftAssignment("1ce3", s1, LocalDate.of(2022, 11, 24)));
-        shiftAssignmentList.add(new ShiftAssignment("1cew4", s2, LocalDate.of(2022, 11, 24)));
-        shiftAssignmentList.add(new ShiftAssignment("1xx5", s3, LocalDate.of(2022, 11, 24)));
-        shiftAssignmentList.add(new ShiftAssignment("1wd6", s4, LocalDate.of(2022, 11, 24)));
-
-        shiftAssignmentList.add(new ShiftAssignment("17cew", s1, LocalDate.of(2022, 11, 25)));
-        shiftAssignmentList.add(new ShiftAssignment("12dwe8", s2, LocalDate.of(2022, 11, 25)));
-        shiftAssignmentList.add(new ShiftAssignment("1ce9", s3, LocalDate.of(2022, 11, 25)));
-        shiftAssignmentList.add(new ShiftAssignment("2cc0", s4, LocalDate.of(2022, 11, 25)));
-
-        return new Schedule(employeeGroupList, shiftAssignmentList);
     }
 
 }
